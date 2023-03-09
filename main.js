baaeaafa2fd454331b9ad555a68dc949957cbd72
ahmed anis <keyloggerpy2022@gmail.com>
2023-03-09 22:09:21 +0300

    popup window completed

---
+++ main.js
@@ -0,0 +1,9 @@
+function closePopUp(){
+    document.getElementById("popUp").style.display= "none";
+    document.getElementById("foot").style.display = "block";
+}
+
+function openPopUp(){
+    document.getElementById("popUp").style.display = "block";
+    document.getElementById("foot").style.display = "none";
+}
