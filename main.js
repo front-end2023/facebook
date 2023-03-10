51eb1178d782be45961f4aaa35ccc83b00e78545
ahmed anis <keyloggerpy2022@gmail.com>
2023-03-10 21:52:37 +0300

    home,index

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+function formValidate() {
+
+    var UserName = document.getElementById("login_email").value;
+    var Password = document.getElementById("login_pass").value;
+    var userError = document.getElementById("userError");
+    var passError = document.getElementById("passError");
+    var text = '';
+
+    if (UserName.length < 6) {
+        //error
+        text = "Please Enter 6 Charcters";
+        userError.innerHTML = text;
+        return false;
+    }
+    else if (isNaN (Password)) {
+        text = "Please Enter a Vailed password";
+        passError.innerHTML = text;
+        return false;
+    }
+    else{
+        alert("Done");
+        return true;
+    }
+}
+
+
+
+
+
+
 function closePopUp(){
     document.getElementById("popUp").style.display= "none";
     document.getElementById("foot").style.display = "block";
@@ -7,3 +37,10 @@ function openPopUp(){
     document.getElementById("popUp").style.display = "block";
     document.getElementById("foot").style.display = "none";
 }
+// onsubmit="return false"
+// validation
+// UserName.indexOf("@") == -1
+
+
+
+
